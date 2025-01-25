810ef6e12ba436172e4ee27c6264d9c5fe30e66e
vegg1ec0de <alexis.alvarez@sintergica.ai>
2025-01-25 12:14:11 -0600

    Refactor NewFolder.svelte SVG structure and update paths
    Simplified SVG formatting by removing redundant attributes and restructuring lines for consistency. Updated path definitions to improve readability and ensure proper rendering. These changes enhance maintainability and align with design requirements.

--- src/lib/components/icons/NewFolder.svelte
+++ src/lib/components/icons/NewFolder.svelte
@@ -2,14 +2,13 @@
 	export let className = 'size-4';
 	export let strokeWidth = '1.5';
 </script>
-<svg
-		 xmlns="http://www.w3.org/2000/svg"
+
+<svg xmlns="http://www.w3.org/2000/svg"
 		 fill="none"
 		 viewBox="0 0 24 24"
 		 stroke-width={strokeWidth}
 		 stroke="currentColor"
 		 class={className}>
-	<path d="M13 9h-2v3H8v2h3v3h2v-3h3v-2h-3z"></path>
-	<path
-		d="M20 5h-8.586L9.707 3.293A.996.996 0 0 0 9 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2zM4 19V7h16l.002 12H4z"></path>
+	<path fill="currentColor"
+				d="M13 19c0 .34.04.67.09 1H4a2 2 0 0 1-2-2V6c0-1.11.89-2 2-2h6l2 2h8a2 2 0 0 1 2 2v5.81c-.61-.35-1.28-.59-2-.72V8H4v10h9.09c-.05.33-.09.66-.09 1m7-1v-3h-2v3h-3v2h3v3h2v-3h3v-2z" />
 </svg>
