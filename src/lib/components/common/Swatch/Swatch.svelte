e9b7cb814469104a025ce21837cbf37dd3239a32
alexis.alvarez@sintergica.ai <myriam.sanchez@sintergica.ai>
2025-02-19 18:10:49 -0600

    Introducing Swatch component and SwatchList

---
+++ src/lib/components/common/Swatch/Swatch.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	type color = 'red' | 'orange' | 'yellow' | 'green' | 'lime' | 'teal' | 'blue' | 'purple' | 'pink';
+	export let color = 'red';
+	export let isSelected = false;
+
+	$: containerClass = `inline-flex p-xs rounded-sm hover:bg-brand-100 cursor-pointer ${
+		isSelected ? 'bg-brand-200' : ''
+	}`;
+
+	$: circleClass = `w-6 h-6 rounded-full border-2 bg-${color}-200 border-${color}-500`;
+
+</script>
+<div class={`${containerClass}`} role="button" on:click>
+	<div class={circleClass}></div>
+</div>
---
+++ src/lib/components/common/Swatch/SwatchList.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Swatch from '$lib/components/common/Swatch/Swatch.svelte';
+	type color = 'lime' | 'red' | 'blue' | 'yellow' | 'green' | 'gray' | 'purple' | 'pink' | 'indigo' | 'teal' | 'orange' | 'rose';
+
+
+	let selectedColor = '';
+
+	function handleSwatchClick(color: color) {
+		selectedColor = color;
+	}
+</script>
+
+<div class="flex flex-wrap gap-sm justify-end">
+	{#each ['red', 'orange', 'yellow', 'green', 'lime', 'teal', 'blue', 'purple', 'pink'] as color}
+		<Swatch
+			{color}
+			isSelected={selectedColor === color}
+			on:click={()=>handleSwatchClick(color)}
+		/>
+	{/each}
+</div>
