411f4c32c35104c2bd92fe40508d7c0d856bea01
Alexis Alvarez <alexis.alvarez@sintergica.ai>
2025-03-05 11:34:58 -0600

    Added create, edit and overview models from legacy interface

---
+++ src/routes/dev/models/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+	import { models } from '$lib/stores';
+	import { getModels } from '$lib/apis';
+	import Models from '$lib/components/workspace/Models.svelte';
+	import ModelsRedesign from '$lib/components/workspace/ModelsRedesign.svelte';
+
+	onMount(async () => {
+		await Promise.all([
+			(async () => {
+				models.set(await getModels(localStorage.token));
+			})()
+		]);
+	});
+</script>
+
+{#if $models !== null}
+	<ModelsRedesign />
+{/if}
