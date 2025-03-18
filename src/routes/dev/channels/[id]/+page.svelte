f2b94754319e81448c691e15561d41b602082930
Alexis Alvarez <alexis.alvarez@sintergica.ai>
2025-03-18 11:52:08 -0600

    Initializing channel redesign

---
+++ src/routes/dev/channels/[id]/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import ChannelRedesign from '$lib/components/channel/ChannelRedesign.svelte';
+</script>
+
+<ChannelRedesign id={$page.params.id} />
