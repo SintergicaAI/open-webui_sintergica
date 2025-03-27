50f5162eaf59ea8cb04c07a0419cd20d67a7c1be
Alexis Alvarez <alexis.alvarez@sintergica.ai>
2025-03-27 13:07:22 -0600

    Add new Svelte icon components
    Introduce new icon components: TuringFace, MessageCircleDashedPlus, Target, TuringFaceWinkyX, and TuringFaceOpenMouth. These components provide scalable and reusable SVG icons for UI integration, with some placeholders for dynamic props.

---
+++ src/lib/components/icons/MessageCircleDashedPlus.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let size: string = '20';
+	export let className: string = '';
+</script>
+<svg width="{size}" height="{size}" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g id="size=base, icon=temporal-message" clip-path="url(#clip0_87_411)">
+			<path id="Vector" d="M6.6665 9.99213H13.3332" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+			<path id="Vector_2" d="M10 6.65881V13.3255" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+			<path id="Vector_3" d="M11.25 2.57546C10.8333 2.57546 10.4167 2.49213 10 2.49213C9.58333 2.49213 9.16667 2.57546 8.75 2.57546" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+			<path id="Vector_4" d="M16.083 5.65863C15.586 4.99496 14.9967 4.40568 14.333 3.90863" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+			<path id="Vector_5" d="M17.417 11.2421C17.5003 10.8255 17.5003 10.4088 17.5003 9.99213C17.5003 9.57546 17.417 9.15879 17.417 8.74213" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+			<path id="Vector_6" d="M14.333 16.0756C14.9967 15.5786 15.586 14.9893 16.083 14.3256" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+			<path id="Vector_7" d="M8.75 17.4086C9.16667 17.492 9.58333 17.492 10 17.492C10.4167 17.492 10.8333 17.4086 11.25 17.4086" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+			<path id="Vector_8" d="M2.91699 14.5756L1.66699 18.3256L5.41699 17.0756" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+			<path id="Vector_9" d="M2.58333 8.74213C2.58333 9.15879 2.5 9.57546 2.5 9.99213C2.5 10.4088 2.58333 10.8255 2.58333 11.2421" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+			<path id="Vector_10" d="M5.66699 3.90863C5.00332 4.40568 4.41404 4.99496 3.91699 5.65863" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+		</g>
+		<defs>
+			<clipPath id="clip0_87_411">
+				<rect width="20" height="20" fill="currentColor" transform="translate(0 -0.00787354)"/>
+			</clipPath>
+		</defs>
+</svg>
+
+
